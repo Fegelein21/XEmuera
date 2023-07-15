0e585fcb9baa0ffb522a1f0a29cd34c020ddb478
CK_Rainbow <nanashichi@proton.me>
2023-07-15 15:36:30 +0800

    update: 更新至EMv8b+EEv15

--- XEmuera/XEmuera/Emuera/GameData/Function/Creator.Method.cs
+++ XEmuera/XEmuera/Emuera/GameData/Function/Creator.Method.cs
@@ -31,7 +31,7 @@ namespace MinorShift.Emuera.GameData.Function
 			{
 				ReturnType = typeof(Int64);
 				argumentTypeArray = null;
-				CanRestructure = false;
+				CanRestructure = true;
 			}
 			public override string CheckArgumentType(string name, IOperandTerm[] arguments)
 			{
@@ -248,7 +248,7 @@ namespace MinorShift.Emuera.GameData.Function
 			{
 				ReturnType = typeof(Int64);
 				argumentTypeArray = new Type[] { typeof(string) };
-				CanRestructure = true;
+				CanRestructure = false;
 			}
 
 			public override Int64 GetIntValue(ExpressionMediator exm, IOperandTerm[] arguments)
@@ -277,7 +277,7 @@ namespace MinorShift.Emuera.GameData.Function
 			{
 				ReturnType = typeof(string);
 				argumentTypeArray = new Type[] { typeof(string) };
-				CanRestructure = true;
+				CanRestructure = false;
 			}
 			public override string GetStrValue(ExpressionMediator exm, IOperandTerm[] arguments)
 			{
@@ -660,19 +660,19 @@ namespace MinorShift.Emuera.GameData.Function
 		{
 			public HtmlSubStringMethod()
 			{
-				ReturnType = typeof(Int64);
+				ReturnType = typeof(string);
 				argumentTypeArray = new Type[] { typeof(string), typeof(Int64) };
 				CanRestructure = false;
 			}
 
-			public override Int64 GetIntValue(ExpressionMediator exm, IOperandTerm[] arguments)
+			public override string GetStrValue(ExpressionMediator exm, IOperandTerm[] arguments)
 			{
 				string str = arguments[0].GetStrValue(exm);
 				string[] strs = MinorShift.Emuera.GameView.HtmlManager.HtmlSubString(str, (int)arguments[1].GetIntValue(exm));
 				string[] output = GlobalStatic.Process.VEvaluator.RESULTS_ARRAY;
 				int outputlength = Math.Min(output.Length, strs.Length);
 				Array.Copy(strs, output, outputlength);
-				return 1;
+				return output[0];
 			}
 		}
 		private sealed class RegexpMatchMethod : FunctionMethod
@@ -4008,7 +4008,7 @@ namespace MinorShift.Emuera.GameData.Function
 				// argumentTypeArray = new Type[] { typeof(string), typeof(string), typeof(string) };
 				argumentTypeArray = null;
 				HasUniqueRestructure = true;
-				CanRestructure = true;
+				CanRestructure = false;
 			}
 
 			public override bool UniqueRestructure(ExpressionMediator exm, IOperandTerm[] arguments)
